f0ae8077884968a30e6deca90b2e5310ea97e254
CodeSandbox <support@codesandbox.io>
2025-06-19 15:28:29 +0000

    d`el

--- app/domains/analytics/controllers/analytics_controller_admin.py
+++
@@ -1,9 +0,0 @@
-"""
-Modul ini sekarang hanya berperan sebagai re-export dari admin controllers.
-Implementasi telah dipindahkan ke modul terpisah di folder admin/.
-"""
-
-from app.domains.analytics.controllers.admin import admin_analytics_router
-
-# Re-export admin_analytics_router
-__all__ = ['admin_analytics_router']
